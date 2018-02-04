285be42c3553e147a2b7633a01c879286d2a8ef5
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2018-02-04 11:34:08 +0100

    Do not use equals function when refering to an agent

--- MaDKitLanEdition/src/main/java/com/distrimind/madkit/kernel/InternalRole.java
+++ MaDKitLanEdition/src/main/java/com/distrimind/madkit/kernel/InternalRole.java
@@ -295,9 +295,10 @@ class InternalRole implements Serializable {// TODO test with arraylist
 	 */
 	boolean addMember(final AbstractAgent requester, boolean manually_requested) {
 		synchronized (players) {
-			if (players.contains(requester)) {// TODO looks like I should use linkedhashset
-				return false;
-			}
+			for (AbstractAgent aa : players)// TODO looks like I should use linkedhashset
+				if (aa==requester)
+					return false;
+			
 			players.add(requester);
 			if (logger != null) {
 				logger.finest(requester.getName() + " is now playing " + getCGRString(group, roleName));
@@ -314,6 +315,7 @@ class InternalRole implements Serializable {// TODO test with arraylist
 		// So addToOverlookers(requester); has to be called in group
 		if (manually_requested)
 			incrementReferences(null);
+
 
 		return true;
 	}
@@ -359,7 +361,17 @@ class InternalRole implements Serializable {// TODO test with arraylist
 
 	ReturnCode removeMember(final AbstractAgent requester, boolean manually_requested) {
 		synchronized (players) {
-			if (!players.remove(requester)) {
+			boolean removed=false;
+			for (Iterator<AbstractAgent> it=players.iterator();it.hasNext();)
+			{
+				if (it.next()==requester)
+				{
+					it.remove();
+					removed=true;
+					break;
+				}
+			}
+			if (!removed) {
 				if (myGroup.isIn(requester)) {
 					return ROLE_NOT_HANDLED;
 				}
@@ -383,9 +395,22 @@ class InternalRole implements Serializable {// TODO test with arraylist
 	final void removeMembers(final List<AbstractAgent> bucket, boolean manually_requested) {
 		int number = 0;
 		synchronized (players) {
-			number = players.size();
-			players.removeAll(bucket);// is optimized
-			number = players.size() - number;
+			number = 0;
+			for (AbstractAgent aa : bucket)
+			{
+				for (Iterator<AbstractAgent> it=players.iterator();it.hasNext();)
+				{
+				
+					if (it.next()==aa)
+					{
+						it.remove();
+						++number;
+						
+						break;
+					}
+				}
+			}
+			
 			if (agentAddresses != null) {
 
 				for (Iterator<AgentAddress> i = agentAddresses.iterator(); i.hasNext();) {

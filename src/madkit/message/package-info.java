3ac6179d4ad3bc16e48a769a73b244bb647671e3
Fabien Michel <fmichel@lirmm.fr>
2018-07-05 22:16:15 +0200

    fixing ugly typos

--- src/madkit/message/package-info.java
+++ src/madkit/message/package-info.java
@@ -36,7 +36,7 @@ knowledge of the CeCILL-C license and that you accept its terms.
  */
 /**
 * Contains subclasses of {@link madkit.kernel.Message} which could be easily reused.
-* This classes could be use at will by MaDKit agents to communicate.
+* These classes could be used at will by MaDKit agents to communicate.
 * The simplest one is the {@link madkit.message.ObjectMessage} class which 
 * can convey any kind of Object. 
 * Others are related to the Multi-Agent world in general.
